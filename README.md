77880494ecd5a386931a7e78ecabad86e89ef0ed
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-03-22 06:53:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Stock no negative with french translation
+-----------------------------------------
+This module add the french translation to __stock_no_negative__ module.
+
---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*- 
+
+
+{
+    'name': 'Stock no negative with french translation',
+    'author': 'Soft-integration',
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'qweb': [],
+    'description': False,
+    'images': [],
+    'version': '1.0.1',
+    'category': 'Stock',
+    'demo': [],
+    'depends': ['stock_no_negative'],
+    'data': [
+    ],
+    'license': 'LGPL-3',
+}
